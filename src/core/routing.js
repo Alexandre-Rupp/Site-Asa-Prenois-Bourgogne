74527642d74f3dbe3524b6ff9064fee69792dea4
Alexandre Rupp <154376208+Alexandre-Rupp@users.noreply.github.com>
2026-04-04 19:48:00 +0200

    perfect

--- src/core/routing.js
+++ src/core/routing.js
@@ -331,7 +331,7 @@ export function updateDocumentSeo(
     meetings,
     canonicalOrigin = "https://www.asa-prenois-bourgogne.org",
     siteName = "ASA Prenois Bourgogne",
-    socialImagePath = "/assets/logo-asa-prenois-bourgogne-new.png?v=20260404-4",
+    socialImagePath = "/assets/logo-asa-prenois-bourgogne-fit.png?v=20260404-7",
   }
 ) {
   const seo = getSeoPayload(route, {
